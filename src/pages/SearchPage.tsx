8a1a7851eba05b52145df1487b348b34e57b1901
xaliz-06 <vaibhavchakraborty06@gmail.com>
2024-05-14 23:12:35 +0530

    fixed search bug

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -30,10 +30,12 @@ const SearchPage = () => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   if (isLoading) {
-    <div className="p-4 mx-auto my-2 text-blue-500 flex flex-col justify-center items-center gap-4">
-      <span className="text-3xl">Loading...</span>
-      <Loader2 className="w-10 h-10" />
-    </div>;
+    return (
+      <div className="p-4 mx-auto my-2 text-blue-500 flex flex-col justify-center items-center gap-4">
+        <span className="text-3xl">Loading...</span>
+        <Loader2 className="w-10 h-10" />
+      </div>
+    );
   }
 
   if (!results?.data || !city) {
